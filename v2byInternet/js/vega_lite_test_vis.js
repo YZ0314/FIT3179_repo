100ee9ad1c441e32da27d3583eb809bea2fe46cf
YZ0314 <92435353+YZ0314@users.noreply.github.com>
2021-10-18 18:07:53 +1100

    Add files via upload

---
+++ v2byInternet/js/vega_lite_test_vis.js
@@ -0,0 +1,5 @@
+
+    var specVis1 = "js/internet_1.vg.json";
+    vegaEmbed("#v1", specVis1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
