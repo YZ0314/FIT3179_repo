41cfc29edba3d358fc61db514425e14606129b40
YZ0314 <92435353+YZ0314@users.noreply.github.com>
2021-10-18 18:08:46 +1100

    Delete v2byInternet directory

--- v2byInternet/js/vega_lite_test_vis.js
+++
@@ -1,5 +0,0 @@
-
-    var specVis1 = "js/internet_1.vg.json";
-    vegaEmbed("#v1", specVis1).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
